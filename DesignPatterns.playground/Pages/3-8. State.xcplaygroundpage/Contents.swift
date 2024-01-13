5fd18338725ec9f47577ac00f60805e7fee4408d
Avery <hellohezhili@gmail.com>
2024-01-13 17:25:50 +0800

    update

--- DesignPatterns.playground/Pages/3-8. State.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/3-8. State.xcplaygroundpage/Contents.swift
@@ -1,14 +1,84 @@
 //: [Previous](@previous)
 
-//: ## 状态模式
-//: 允许一个对象在其内部状态发生改变时改变它的行为, 对象看起来似乎修改了它的类
+import AVFoundation
 
-//: ### 参与者
+/// State
+protocol PlayerStateType: AnyObject {
+    func primaryAction()
+    /// Optional
+    func handleInterruptBegan()
+    func handleInterruptEnded()
+}
 
+extension PlayerStateType {
+    func handleInterruptBegan() {}
+    func handleInterruptEnded() {}
+}
 
-//: ### 代码示例
-import Foundation
+/// Context
+final class PlayerManager {
+    var state: PlayerStateType? {
+        didSet { state?.primaryAction() }
+    }
+    
+    func handleInterruptNotification(_ interruptType: AVAudioSession.InterruptionType) {
+        switch interruptType {
+        case .began:
+            state?.handleInterruptBegan()
+        case .ended:
+            state?.handleInterruptEnded()
+        }
+    }
+}
 
-var greeting = "Hello, playground"
+/// ConcreteState
+final class PlayState: PlayerStateType {
+    private(set) unowned var manager: PlayerManager
+    
+    init?(_ manager: PlayerManager) {
+        self.manager = manager
+    }
+    
+    func handleInterruptBegan() {
+        print("打断播放")
+        manager.state = InterruptState(manager)
+    }
+    
+    func primaryAction() {
+        print("player: 播放")
+    }
+    
+}
+
+class PauseState: PlayerStateType {
+    private(set) unowned var manager: PlayerManager
+    
+    init?(_ manager: PlayerManager) {
+        self.manager = manager
+    }
+    
+    func primaryAction() {
+        print("player: 暂停")
+    }
+    
+    func handleInterruptEnded() {}
+}
+
+final class InterruptState: PauseState {
+    override func handleInterruptEnded() {
+        print("恢复播放")
+        manager.state = PlayState(manager)
+    }
+}
+
+/// Client
+let manager = PlayerManager()
+
+manager.handleInterruptNotification(.began)
+manager.state = PlayState(manager)
+
+manager.handleInterruptNotification(.ended)
+manager.handleInterruptNotification(.began)
+manager.handleInterruptNotification(.ended)
 
 //: [Next](@next)
