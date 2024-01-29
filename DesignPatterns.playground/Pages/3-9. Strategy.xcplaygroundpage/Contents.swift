adf982b7a9f4aa830157514405727954be7207ee
Avery <hellohezhili@gmail.com>
2024-01-29 14:26:30 +0800

    Update

--- DesignPatterns.playground/Pages/3-9. Strategy.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/3-9. Strategy.xcplaygroundpage/Contents.swift
@@ -1,14 +1,49 @@
 //: [Previous](@previous)
 
-//: ## 策略模式
-//: 定义一系列的算法, 把它们一个个封装起来, 并且使它们可相互替换, 策略模式使得算法可以独立于使用它的客户而变化
-
-//: ### 参与者
-
-
-//: ### 代码示例
 import Foundation
 
-var greeting = "Hello, playground"
+/// Strategy
+protocol ErrorHandlerType: AnyObject {
+    func handle(error: Error)
+}
+
+/// ConcreteStrategy
+final class DebugErrorHandler: ErrorHandlerType {
+    func handle(error: Error) {
+        print("Show alert for error: \(error.localizedDescription)")
+    }
+}
+
+final class ReleaseErrorHandler: ErrorHandlerType {
+    func handle(error: Error) {
+        print("Write error to log file: \(error.localizedDescription)")
+    }
+}
+
+/// Context
+final class Controller {
+    let errorHandler: ErrorHandlerType
+    
+    init(_ errorHandler: ErrorHandlerType) {
+        self.errorHandler = errorHandler
+    }
+    
+    func request() {
+        do {
+            // NSLocalizedDescriptionKey, NSUnderlyingErrorKey
+            throw NSError(domain: "com.dp.test", code: -1, userInfo: [NSLocalizedDescriptionKey: "请求出错!"])
+        } catch {
+            errorHandler.handle(error: error)
+        }
+    }
+}
+
+/// Client
+
+let debugController = Controller(DebugErrorHandler())
+debugController.request()
+
+let releaseController = Controller(ReleaseErrorHandler())
+releaseController.request()
 
 //: [Next](@next)
