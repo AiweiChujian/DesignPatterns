b2639396a8244da582f8ad5dcec37b7d0897131c
Avery <hellohezhili@gmail.com>
2024-03-01 17:39:59 +0800

    Update

--- DesignPatterns.playground/Pages/3-11. Visitor.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/3-11. Visitor.xcplaygroundpage/Contents.swift
@@ -1,14 +1,82 @@
 //: [Previous](@previous)
 
-//: ## 访问者模式
-//: 表示一个作用于某对象结构中的各元素的操作, 它使你可以在不改变各元素的类的前提下定义作用于这些元素的新操作
-
-//: ### 参与者
-
-
-//: ### 代码示例
 import Foundation
 
-var greeting = "Hello, playground"
+/// ObjectStructure
+final class SingRobot {
+    private let disk = Disk(content: "两只老虎")
+    
+    private let speaker = Speaker(tone: "电子音")
+    
+    func sing() {
+        print("用[\(speaker.tone)]唱歌:《\(disk.content)》")
+    }
+}
+
+final class Disk {
+    var content: String
+    
+    init(content: String) {
+        self.content = content
+    }
+}
+
+final class Speaker {
+    var tone: String
+    
+    init(tone: String) {
+        self.tone = tone
+    }
+}
+
+/// Visitor
+protocol Visitor {
+    func visit(_  disk: Disk)
+    func visit(_ speaker: Speaker)
+}
+
+/// ConcreteVisitor
+struct UpdateVistor: Visitor {
+    func visit(_ disk: Disk) {
+        disk.content = "发如雪"
+    }
+    
+    func visit(_ speaker: Speaker) {
+        speaker.tone = "拟人声"
+    }
+}
+
+/// Element
+protocol Visitable {
+    func accept(_ visitor: Visitor)
+}
+
+/// ConcreteElement
+extension Disk: Visitable {
+    func accept(_ visitor: Visitor) {
+        visitor.visit(self)
+    }
+}
+
+extension Speaker: Visitable {
+    func accept(_ visitor: Visitor) {
+        visitor.visit(self)
+    }
+}
+
+extension SingRobot: Visitable {
+    func accept(_ visitor: Visitor) {
+        disk.accept(visitor)
+        speaker.accept(visitor)
+    }
+}
+
+let robot = SingRobot()
+robot.sing()
+
+let updater = UpdateVistor()
+robot.accept(updater)
+
+robot.sing()
 
 //: [Next](@next)
